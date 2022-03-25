78d7d1c7d3ccb6235c228020f4180ec486788381
zeggel <wickac@bk.ru>
2022-03-25 23:30:42 +0300

    Deleted debug lines

--- src/GameObject.lua
+++ src/GameObject.lua
@@ -34,8 +34,4 @@ end
 
 function GameObject:render()
     love.graphics.draw(gTextures[self.texture], gFrames[self.texture][self.frame], self.x, self.y)
-
-    love.graphics.setColor(1, 0, 0, 1)
-    love.graphics.rectangle("line", self.x, self.y, self.width, self.height)
-    love.graphics.setColor(1, 1, 1, 1)
 end
